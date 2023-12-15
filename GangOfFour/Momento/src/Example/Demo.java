64474b8afda5d403467a089c35ccf5a00c898ba5
AbubakrKaakar <92436704+AbubakrKaakar@users.noreply.github.com>
2023-12-15 10:46:14 +0500

    Memento added

---
+++ GangOfFour/Momento/src/Example/Demo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Example;
+import java.awt.*;
+/**
+ *
+ * @author sp20-bse-072
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Editor editor = new Editor();
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+    
+}
---
+++ GangOfFour/Momento/src/LabWork/Command.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LabWork;
+
+import Example.*;
+
+/**
+ *
+ * @author sp20-bse-072
+ */
+public interface Command {
+    String getName();
+    void execute();
+}
